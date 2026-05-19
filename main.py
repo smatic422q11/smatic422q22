2f5fa689ecc651fa5e33dae53d7581617e421e62
Goran Petrovic <mmcommunity22@gmail.com>
2026-05-19 14:17:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -516,5 +516,5 @@ async def get_sector_text(sector_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000)) 
     uvicorn.run(app, host="0.0.0.0", port=port)  
